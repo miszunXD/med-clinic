7818314018a8e1527a4ffda1fca608dfcec07530
miszunXD <filip.karpinski480@gmail.com>
2026-07-31 17:43:33 +0200

    Implement doctor schedule TreeSet

--- src/main/java/com/miszunXD/medclinic/model/Appointment.java
+++ src/main/java/com/miszunXD/medclinic/model/Appointment.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 
 public record Appointment(String appointmentId, String doctorId, String patientPesel,
                           LocalDateTime dateTime, boolean isCancelled, double price)
-        implements Identifiable<String> {
+        implements Identifiable<String>, Comparable<Appointment>{
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -28,5 +28,16 @@ public record Appointment(String appointmentId, String doctorId, String patientP
     @JsonIgnore
     public String getId() {
         return appointmentId;
+    }
+
+    @Override
+    public int compareTo(Appointment o) {
+        int dateComparison =  this.dateTime.compareTo(o.dateTime);
+
+        if (dateComparison != 0) {
+            return dateComparison;
+        }
+
+        return this.appointmentId.compareTo(o.appointmentId);
     }
 }
